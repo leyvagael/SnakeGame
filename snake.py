55cba8f76f96bad63c900e34ebed33e162de8968
leyvagael <leyva.gael@gmail.com>
2026-05-07 00:59:23 -0600

    Added random movement to the food every 200ms, and calling the funtion that does it in the main loop.

--- snake.py
+++ snake.py
@@ -35,15 +35,27 @@ def random_food_color():
 
 food_color = random_food_color()
 
+def move_food():
+    directions = [vector(10, 0), vector(-10, 0), vector(0, 10), vector(0, -10)]
+    step = choice(directions)
+    new_x = food.x + step.x
+    new_y = food.y + step.y
+
+    if -150 <= new_x <= 140 and -150 <= new_y <= 140:
+        food.x = new_x
+        food.y = new_y
+
+    ontimer(move_food, 200)
+
 def change(x, y):
     """Change snake direction."""
     aim.x = x
     aim.y = y
 
 
-def inside(head):
-    """Return True if head inside boundaries."""
-    return -200 < head.x < 190 and -200 < head.y < 190
+def inside(obj):
+    """Return True if object inside boundaries."""
+    return -200 < obj.x < 190 and -200 < obj.y < 190
 
 def mouse(x, y):
     """Control snake with mouse clicks."""
@@ -69,6 +81,7 @@ def move():
     head = snake[-1].copy()
     head.move(aim)
 
+
     if not inside(head) or head in snake:
         square(head.x, head.y, 9, 'red')
         update()
@@ -93,7 +106,6 @@ def move():
     update()
     ontimer(move, 76)
 
-
 setup(420, 420, 370, 0)
 hideturtle()
 tracer(False)
@@ -104,4 +116,5 @@ onkey(lambda: change(-10, 0), 'Left')
 onkey(lambda: change(0, 10), 'Up')
 onkey(lambda: change(0, -10), 'Down')
 move()
+move_food()
 done()
